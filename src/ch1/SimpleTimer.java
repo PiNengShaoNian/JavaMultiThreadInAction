cb3d67f6d75ac2bea42688a8fc9e3ab2d8c996cf
quguiyou <quguiyou1076@qq.com>
2024-06-25 22:28:46 +0800

    SimpleTimer

---
+++ src/ch1/SimpleTimer.java
@@ -0,0 +1,29 @@
+package ch1;
+
+public class SimpleTimer {
+    private static int count;
+
+    public static void main(String[] args) {
+        count = args.length >= 1 ? Integer.valueOf(args[0]) : 60;
+        int remaining;
+        while (true) {
+            remaining = countDown();
+            if (remaining == 0) {
+                break;
+            } else {
+                System.out.println("Remaining " + count + " second(s)");
+            }
+
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                // 什么也不做;
+            }
+        }
+        System.out.println("Done.");
+    }
+
+    private static int countDown() {
+        return count--;
+    }
+}
