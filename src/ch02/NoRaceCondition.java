ff816c78770e00ac26bcd713f34f81558eda988b
quguiyou <quguiyou1076@qq.com>
2024-06-27 22:40:15 +0800

    NoRaceCondition

---
+++ src/ch02/NoRaceCondition.java
@@ -0,0 +1,13 @@
+package ch02;
+
+public class NoRaceCondition {
+    public int nextSequence(int sequence) {
+        // 以下语句使用的是局部变量而非状态变量，并不会产生竞态
+        if (sequence >= 999) {
+            sequence = 0;
+        } else {
+            sequence++;
+        }
+        return sequence;
+    }
+}
