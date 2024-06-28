4ce935a8982c86f7eb55610f2f83f734da3fdbd9
quguiyou <quguiyou1076@qq.com>
2024-06-28 20:44:05 +0800

    SafeCircularSeqGenerator

---
+++ src/ch02/SafeCircularSeqGenerator.java
@@ -0,0 +1,15 @@
+package ch02;
+
+public class SafeCircularSeqGenerator implements CircularSeqGenerator {
+    private short sequence = -1;
+
+    @Override
+    public synchronized short nextSequence() {
+        if (sequence >= 999) {
+            sequence = 0;
+        } else {
+            sequence++;
+        }
+        return sequence;
+    }
+}
