d73865fccc3cdd70e5e56c1a52d5fb18e6b18239
quguiyou <quguiyou1076@qq.com>
2024-06-30 15:01:17 +0800

    SingleThreadedSingleton

---
+++ src/ch03/SingleThreadedSingleton.java
@@ -0,0 +1,31 @@
+package ch03;
+
+// 线程不安全的单例模式
+public class SingleThreadedSingleton {
+    // 保存该类的唯一实例
+    private static SingleThreadedSingleton instance = null;
+
+    // 省略实例变量声明
+    /*
+     * 私有构造器使其他类无法直接通过new创建该类的实例
+     */
+    private SingleThreadedSingleton() {
+        // 什么也不做
+    }
+
+    /**
+     * 创建并返回该类的唯一实例 <BR>
+     * 即只有该方法被调用时该类的唯一实例才会被创建
+     *
+     * @return
+     */
+    public static SingleThreadedSingleton getInstance() {
+        if (instance == null) {
+            instance = new SingleThreadedSingleton();
+        }
+        return instance;
+    }
+
+    public void someService() {
+    }
+}
