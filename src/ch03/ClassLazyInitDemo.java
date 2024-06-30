401648880a96ca3aa8c4c0aa7d3956d28c4c8494
quguiyou <quguiyou1076@qq.com>
2024-06-30 16:08:27 +0800

    ClassLazyInitDemo

---
+++ src/ch03/ClassLazyInitDemo.java
@@ -0,0 +1,19 @@
+package ch03;
+
+import util.Debug;
+
+public class ClassLazyInitDemo {
+    public static void main(String[] args) {
+        Debug.info(Collaborator.class.hashCode());// 语句①
+        Debug.info(Collaborator.number);// 语句②
+        Debug.info(Collaborator.flag);
+    }
+
+    static class Collaborator {
+        static int number = 1;
+        static boolean flag = true;
+        static {
+            Debug.info("Collaborator initializing...");
+        }
+    }
+}
