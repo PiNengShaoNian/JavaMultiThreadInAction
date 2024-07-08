28dd926ce0e93c8269fc9c82037a7e106b580de9
quguiyou <quguiyou1076@qq.com>
2024-07-08 19:41:32 +0800

    SmsVerificationCodeSender

---
+++ src/ch6/case01/SmsVerificationCodeSender.java
@@ -0,0 +1,22 @@
+package ch6.case01;
+
+import java.text.DecimalFormat;
+
+public class SmsVerificationCodeSender {
+    /**
+     * 生成并下发验证码短信到指定的手机号码。
+     *
+     * @param msisdn 短信接收方号码。
+     */
+    public void sendVerificationSms(final String msisdn) {
+        String verificationCode = generateVerificationCode();
+        // 省略其他代码
+    }
+
+    private String generateVerificationCode() {
+        // 生成强随机数验证码
+        int verificationCode = ThreadSpecificSecureRandom.INSTANCE.nextInt(999999);
+        DecimalFormat df = new DecimalFormat("000000");
+        return df.format(verificationCode);
+    }
+}
